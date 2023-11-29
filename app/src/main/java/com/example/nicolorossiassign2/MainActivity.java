9e08d47fb927efc405e2c0f38af423e4c273da7d
NicoloRossiDCU <nicolo.rossi2@mail.dcu.ie>
2023-11-29 14:48:45 +0100

    Initial commit with basic UI view

---
+++ app/src/main/java/com/example/nicolorossiassign2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nicolorossiassign2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
