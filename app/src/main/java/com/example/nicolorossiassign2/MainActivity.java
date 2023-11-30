a5071bb33bc43236a4d6dbc6f514314090a438b8
NicoloRossiDCU <nicolo.rossi2@mail.dcu.ie>
2023-11-30 10:44:02 +0100

    Added intents for first two text views

--- app/src/main/java/com/example/nicolorossiassign2/MainActivity.java
+++ app/src/main/java/com/example/nicolorossiassign2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.nicolorossiassign2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void implicitIntentCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivity(intent);
+    }
+
+    public void implicitIntentGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivity(intent);
     }
 }
