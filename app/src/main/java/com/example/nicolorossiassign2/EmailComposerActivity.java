e56122b8af2b57d9aa533c48bcefcf914fd38476
NicoloRossiDCU <nicolo.rossi2@mail.dcu.ie>
2023-12-03 15:41:15 +0100

    Added last functionality

--- app/src/main/java/com/example/nicolorossiassign2/EmailComposerActivity.java
+++ app/src/main/java/com/example/nicolorossiassign2/EmailComposerActivity.java
@@ -1,13 +1,37 @@
 package com.example.nicolorossiassign2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EmailComposerActivity extends AppCompatActivity {
+    public final static String MESSAGE_TO = "com.example.buildsimpleuinicolorossiassign2.MESSAGE_TO";
+    public final static String MESSAGE_SUBJECT = "com.example.buildsimpleuinicolorossiassign2.MESSAGE_SUBJECT";
+    public final static String MESSAGE_CONTENT = "com.example.buildsimpleuinicolorossiassign2.MESSAGE_CONTENT";
 
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_composer);
+    }
+
+    public void save(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editTextTo = (EditText) findViewById(R.id.editTextTo);
+        EditText editTextSubject = (EditText) findViewById(R.id.editTextSubject);
+        EditText editTextContent = (EditText) findViewById(R.id.editTextContent);
+
+        String messageTo = editTextTo.getText().toString();
+        String messageSubject = editTextSubject.getText().toString();
+        String messageContent = editTextContent.getText().toString();
+        intent.putExtra(MESSAGE_TO, messageTo);
+        intent.putExtra(MESSAGE_SUBJECT, messageSubject);
+        intent.putExtra(MESSAGE_CONTENT, messageContent);
+        setResult(RESULT_OK, intent);
+        finish();
+
+
     }
 }
--- app/src/main/java/com/example/nicolorossiassign2/MainActivity.java
+++ app/src/main/java/com/example/nicolorossiassign2/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.nicolorossiassign2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.MailTo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static String emailTo;
+    private static String emailContent;
+    private static String emailSubject;
+    ActivityResultLauncher<Intent> getMessage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    emailTo = result.getData().getStringExtra(EmailComposerActivity.MESSAGE_TO);
+                    emailContent = result.getData().getStringExtra(EmailComposerActivity.MESSAGE_CONTENT);
+                    emailSubject = result.getData().getStringExtra(EmailComposerActivity.MESSAGE_SUBJECT);
+                    String emailComplete= String.format("To: %s\nSubject: %s\nContent: %s\n", emailTo, emailSubject, emailContent);
+                    if(result != null
+                            && emailTo != null
+                            &&  emailContent != null
+                            && emailSubject != null){
+                        TextView Email = (TextView) findViewById(R.id.textView4);
+                        Email.setText(emailComplete);
+                        Button button = (Button) findViewById(R.id.buttonSend);
+                        button.setEnabled(true);
 
+                    }
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +63,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void explicitIntentComposer(View view) {
         Intent intent = new Intent(this, EmailComposerActivity.class);
+
+        getMessage.launch(intent);
+    }
+
+
+    public void sendEmail(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri data = Uri.parse("mailto:" + emailTo + "?subject=" + emailSubject + "&body=" + emailContent);
+
+        intent.setData(data);
         startActivity(intent);
+
     }
 }
